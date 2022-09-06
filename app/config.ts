9dcb82a5cc236cc5dae6e989edb3436b5e47d3be
Alexis <alexis.oney@gmail.com>
2022-09-06 22:17:39 +0200

    Update config

--- app/config.ts
+++ app/config.ts
@@ -1,15 +1,14 @@
 // Website
-export const SITE_NAME = 'Pelostudio x NextJS x Storyblok'
-export const SITE_URL = 'https://www.pelostudio-dev.com'
+export const SITE_NAME = 'Alexis Oney'
+export const SITE_URL = 'https://alexis-oney.netlifly.app'
 export const THUMBNAIL = 'thumbnail.jpeg'
 
 // Storyblok
-export const STORYBLOK_TOKEN = 'utIuzhmEkgp0ghVtEZsepAtt'
 export const DEFAULT_LOCALE = 'fr'
-export const LOCALES = ['en', 'it']
+export const LOCALES: string[] = []
 
 // Transitions
 export const PAGE_TRANSITION_ENTERING_DURATION = 400
 
 // Analytics
-export const PLAUSIBLE_DOMAIN = 'pelostudio-nextjs-storyblok'
+export const PLAUSIBLE_DOMAIN = 'alexis-oney'
