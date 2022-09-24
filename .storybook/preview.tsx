6293bf509b8a2d1b60df23fdf0c36ceb85a587e5
Alexis <alexis.oney@gmail.com>
2022-09-24 08:46:29 +0200

    Clean up Storybook folder

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,6 @@
-import './storybook.scss'
 import '../src/styles/globals.scss'
 
 import {RouterContext} from 'next/dist/shared/lib/router-context'
-import * as NextImage from 'next/image'
 
 export const parameters = {
   actions: {argTypesRegex: '^on[A-Z].*'},
@@ -15,10 +13,3 @@ export const parameters = {
     Provider: RouterContext.Provider,
   },
 }
-
-const OriginalNextImage = NextImage.default
-
-Object.defineProperty(NextImage, 'default', {
-  configurable: true,
-  value: (props) => <OriginalNextImage {...props} unoptimized />,
-})
